0ab373089905b3f4c8859fc16e2875275d443034
slava-yr <salvadoryabar@hotmail.com>
2024-03-02 13:15:00 -0500

    fixed monitor speed
    115200

--- WhatsApp ESP32/src/main.cpp
+++ WhatsApp ESP32/src/main.cpp
@@ -1,18 +1,68 @@
+/* 
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-send-messages-whatsapp/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
 #include <Arduino.h>
+#include <WiFi.h>    
+#include <HTTPClient.h>
+#include <UrlEncode.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "dlink-3050";
+const char* password = "hjupb35239";
+
+// +international_country_code + phone number
+// Portugal +351, example: +351912345678
+String phoneNumber = "+51943402428";
+String apiKey = "8147172";
+
+void sendMessage(String message){
+
+  // Data to send with HTTP POST
+  String url = "https://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);    
+  HTTPClient http;
+  http.begin(url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Send Message to WhatsAPP
+  sendMessage("Hola Mundo!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
